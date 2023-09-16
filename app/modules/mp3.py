13d2c0c5d7c850d81658335d06d161f49de8fbd1
Julia Kostin <jk@Julias-MacBook-Pro.local>
2023-09-16 23:09:26 +0200

    changed mp3

--- app/modules/mp3.py
+++ app/modules/mp3.py
@@ -2,12 +2,13 @@
 
 import eyed3
 import re
+eyed3.log.setLevel("ERROR")
 
 def is_sensitive(filename):
     audiofile = eyed3.load(filename)
     if audiofile is not None:
-         if audiofile.tag.title or audiofile.tag.artist or re.search('dvd', audiofile.tag.comments[0].text):
+         if audiofile.tag.title or audiofile.tag.artist or (audiofile.tag.comments and re.search('dvd', audiofile.tag.comments[0].text)):
                  return False 
          else:
              return None
-    return False 
+    return False
