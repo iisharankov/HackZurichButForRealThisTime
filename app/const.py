f4e2b159ae1d903f2c5d0621d2a07490dde5d813
Valentin Roth <varoth@student.ethz.ch>
2023-09-17 00:04:07 +0200

    Changes: # Added constants # Added msg.py # Removed phonenumbers package

--- app/const.py
+++ app/const.py
@@ -10,6 +10,8 @@ SENSITIVE_TOKENS = ["First Name", "First name", "first name", "Last Name",
 PATTERN_EMAIL = re.compile(r"\S+@\S+\.\S+")
 PATTERN_IBAN = re.compile(r"(AD|AE|AL|AO|AT|AZ|BA|BE|BF|BG|BH|BI|BJ|BL|BY|BR|CH|CR|CY|CZ|CF|CG||CI|CM|CV|DE|DK|DO|DZ|EE|EG|ES|FI|FO|FR|GA|GB|GE|GI|GF|GL|GP|GR|GT|HR|HU|IE|IL|IQ|IS|IT|JO|KW|KZ|LB|LC|LI|LT|LU|LV|LY|MC|MD|ME|MF|MG|MK|MQ|MR|MT|MU|MZ|NI|NC|NL|NO|PF|PK|PL|PM|PS|PT|QA|RE|RO|RU|RS|SA|SE|SI|SM|SK|ST|TL|TF|TN|TR|VA|VG|WF|XK|YT)\d{16,32}")
 PATTERN_RSA = re.compile(r"(-----BEGIN RSA PRIVATE KEY-----)(\s|\n)*MII(.|\n){10,1616}(-----END RSA PRIVATE KEY-----)")
+PATTERN_NEWLINE = re.compile(r"(?:\r\n|\r|\n)")
+PATTERN_WHITESPACE = re.compile(r"\s\s+")
 
 ENCODINGS = ['utf-8', 'ISO-8859-1', 'windows-1252']
 RE_ITER_LIMIT = 5
