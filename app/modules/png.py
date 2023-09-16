de91875b94a0a8721a0a059023002412791cecbf
Julia Kostin <jk@Julias-MacBook-Pro.local>
2023-09-16 17:49:56 +0200

    jpg and png

--- app/modules/png.py
+++ app/modules/png.py
@@ -1,14 +1,20 @@
 # Python script for png
 
 import easyocr
+import numpy as np
 
 def is_sensitive(filename, detector):
 
     #predownload the Reader !!! 
     reader = easyocr.Reader(['en'])
-    result = reader.readtext('filename', detail = 0)
+    result = reader.readtext(filename, detail = 0)
 
-    result = " ".join(result)
+    counter = np.array([0,0,0,0])
 
-    return detector.is_sensitive(result)
+    for line in result:
+        new_count = np.array(detector.is_sensitive(line))
+        counter += new_count
+        if (counter[0] > 0) or (counter[1]+counter[2] > 1) or (counter[1]+counter[3] > 1):
+            return True 
 
+    return False 
