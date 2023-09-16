ad6aee85c7798fedb2089ddcecfb9d3a681b28cc
yahya010 <49173233+yahya010@users.noreply.github.com>
2023-09-16 20:54:12 +0300

    Update txt.py

--- app/modules/txt.py
+++ app/modules/txt.py
@@ -22,7 +22,23 @@ def read_text_from_file(file_path):
     # If we've tried all encodings and none worked, then the file might contain binary or non-textual data
     return "Unable to decode the file. It may contain non-textual data."
 
+def is_sensitive(filename, detector):
+    # here: produce list of lines  from html 
+
+    doc = read_text_from_file(filename)
+    counter = np.array([0,0,0,0])
+
+    for line in doc:
+        new_count = np.array(detector.is_sensitive(line))
+        counter += new_count 
+        if (counter[0] > 0) or (counter[1]+counter[2] > 1) or (counter[1]+counter[3] > 1):
+            return True 
+
+    return False
+
+return detector.is_sensitive(text)
+
 # Example
-file_path = "/content/begin-wonder-center-consumer-my-task.log"
-extracted_text = read_text_from_file(file_path)
-print(extracted_text)
+# file_path = "/content/begin-wonder-center-consumer-my-task.log"
+# extracted_text = read_text_from_file(file_path)
+# print(extracted_text)
