714b363de9f701d830c6d10c2e2e8d42a41e4fba
Valentin Roth <varoth@student.ethz.ch>
2023-09-16 23:26:03 +0200

    Changes: # Refactored csv.py and xlsx.py

--- app/modules/csv.py
+++ app/modules/csv.py
@@ -11,9 +11,7 @@ def regex_filter(string):
         "iban": re.search(PATTERN_IBAN, string) is not None
     }
 
-def is_sensitive(filename, detector = None): 
-
-    df = pd.read_csv(filename, nrows = 20)
+def check_is_sensitive(df, filename):
 
     # Check 1: Has header
     has_header = not any([not isinstance(col, str) for col in df.columns])
@@ -30,7 +28,11 @@ def is_sensitive(filename, detector = None):
 
     df_string = df.__str__()
     regex = regex_filter(df_string)
-    has_email, has_iban = regex["email"], regex["iban"]
+    has_email, has_iban, has_rsa = regex["email"], regex["iban"], regex["rsa"]
+
+    if has_rsa:
+        # print("Check 2 (RSA)", filename)
+        return True
 
     if has_email and not "git@" in df_string: # Only true if it isn't github email...
         # print("Check 2 (MAIL)", filename)
@@ -41,8 +43,12 @@ def is_sensitive(filename, detector = None):
         return True
     
     # Check 3: Has a sensitive token in it
-    if sum(sens in df_string for sens in SENSITIVE_TOKENS) > 1: 
+    if sum(sens in df_string.lower() for sens in SENSITIVE_TOKENS) > 1: 
         # print("Check 3", filename)
         return True
 
     return False
+
+def is_sensitive(filename, detector = None): 
+    df = pd.read_csv(filename, nrows = 20)
+    return check_is_sensitive(df, filename)
--- app/modules/xlsx.py
+++ app/modules/xlsx.py
@@ -2,47 +2,10 @@
 
 import pandas as pd
 import re
+import csv
 
 from const import *
 
-def regex_filter(string): 
-    return {
-        "email": re.search(PATTERN_EMAIL, string) is not None, 
-        "iban": re.search(PATTERN_IBAN, string) is not None
-    }
-
 def is_sensitive(filename, detector = None): 
-
     df = pd.read_excel(filename, nrows = 20)
-
-    # Check 1: Has header
-    has_header = not any([not isinstance(col, str) for col in df.columns])
-    if has_header: # If there is a header
-
-        headers = df.columns
-        headers = [col.lower().strip() for col in df.columns]
-
-        if sum(sens in headers for sens in SENSITIVE_HEADERS) > 1: 
-            # print("Check 1", filename)
-            return True
-
-    # Check 2: Has email or IBAN
-
-    df_string = df.__str__()
-    regex = regex_filter(df_string)
-    has_email, has_iban = regex["email"], regex["iban"]
-
-    if has_email and not "git@" in df_string: # Only true if it isn't github email...
-        # print("Check 2 (MAIL)", filename)
-        return  True 
-    
-    if has_iban: 
-        # print("Check 2 (IBAN)", filename)
-        return True
-    
-    # Check 3: Has a sensitive token in it
-    if sum(sens in df_string for sens in SENSITIVE_TOKENS) > 1: 
-        # print("Check 3", filename)
-        return True
-
-    return False
+    return csv.check_is_sensitive(df, filename)
