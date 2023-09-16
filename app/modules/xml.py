d1112800fa1cde7d16bbc201386bec44be403f0f
Julia Kostin <jk@Julias-MacBook-Pro.local>
2023-09-16 21:35:47 +0200

    xml

--- app/modules/xml.py
+++ app/modules/xml.py
@@ -4,71 +4,20 @@ import chardet
 import numpy as np
 from xml.etree import cElementTree as ET
 import os 
+from bs4 import BeautifulSoup
 
-def is_sensitive(filename, detector):
+def is_sensitive(filename):
 
-    with open('dict.xml', 'r') as f:
-        data = f.read()
-
-    def read_text_from_file(file_path):
-        """Reads and returns the content of a file using the appropriate encoding."""
-        
-        with open(file_path, 'rb') as file:
-            rawdata = file.read()
-            
-        encoding_detected = chardet.detect(rawdata)['encoding']
-        
-        # List of encodings to try:
-        encodings_to_try = [encoding_detected, 'utf-8', 'ISO-8859-1', 'windows-1252']
-        
-        for encoding in encodings_to_try:
-            try:
-                content = rawdata.decode(encoding)
-                return content
-            except UnicodeDecodeError:
-                pass
-
-        # If we've tried all encodings and none worked, then the file might contain binary or non-textual data
-        return ''
-
-
+    with open(filename, encoding='ISO-8859-1') as f:
+        rawdata = f.read()
     
-    Bs_data = BeautifulSoup(data, "xml")
-    b_unique = Bs_data.find_all('unique')
+    root = ET.fromstring(rawdata)
 
-    for line in plain_text:
-        new_count = np.array(detector.is_sensitive(line))
-        counter += new_count 
-        if (counter[0] > 0) or (counter[1]+counter[2] > 1) or (counter[1]+counter[3] > 1):
-            return True 
-
-    return False
-
-
-
-def read_text_from_file(file_path):
-        """Reads and returns the content of a file using the appropriate encoding."""
-        
-        with open(file_path, 'rb') as file:
-            rawdata = file.read()
-            
-        encoding_detected = chardet.detect(rawdata)['encoding']
-        
-        # List of encodings to try:
-        encodings_to_try = [encoding_detected, 'utf-8', 'ISO-8859-1', 'windows-1252']
-        
-        for encoding in encodings_to_try:
-            try:
-                content = rawdata.decode(encoding)
-                return content
-            except UnicodeDecodeError:
-                pass
-
-        # If we've tried all encodings and none worked, then the file might contain binary or non-textual data
-        return ''
-
-for filename in os.listdir('files'):
-    if '.xml' in filename:
-        content = read_text_from_file('files/'+filename)
-        print(content[:200])
-        print('--------------------')
+    for element in root:
+        firstname = element[0].text
+        lastname = element[1].text
+        address = element[2].text
+        iban = element[3].text 
+        if (firstname and lastname) and(address or iban):
+            return True
+        return False 
