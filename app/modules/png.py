dc6b344de7257e5ace7648d47307d545635c867c
Julia Kostin <jk@Julias-MacBook-Pro.local>
2023-09-16 14:08:55 +0200

    text analysis dummy

--- app/modules/png.py
+++ app/modules/png.py
@@ -1,1 +1,13 @@
 # Python script for png
+
+import cv2
+import pytesseract
+from text_analyzer import text_is_sensitive
+
+def is_sensitive(filename):
+
+    img = cv2.imread(filename)   
+    text = pytesseract.image_to_string(img)
+    
+    return text_is_sensitive(text) 
+
