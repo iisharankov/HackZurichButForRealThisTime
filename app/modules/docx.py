adds helper file and fixes sensititvity calculation

app/modules/docx.py
@@ -4,6 +4,8 @@ import numpy as np
 import subprocess
 import sys
 
+import helpers
+
 def is_sensitive(filename, detector):
     # here: produce list of lines  from html 
     cmd = ["sh", "-c",
@@ -16,7 +18,4 @@ def is_sensitive(filename, detector):
     result.check_returncode()
 
     counter = np.array(detector.is_sensitive(result.stdout))
-    if (counter[0] > 0) or (counter[1]+counter[2] > 1) or (counter[1]+counter[3] > 1):
-        return True 
-
-    return False
+    return helpers.check_valid_sensitivities(counter)

app/modules/html.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import re
 import numpy as np
 
+import helpers
+
 def is_sensitive(filename, detector):
 
     with open(filename, "r", encoding="utf-8") as file:
@@ -14,7 +16,4 @@ def is_sensitive(filename, detector):
     
 
     counter = np.array(detector.is_sensitive(" ".join(text)))
-    if (counter[0] > 0) or (counter[1]+counter[2] > 1) or (counter[1]+counter[3] > 1):
-        return True 
-
-    return False
+    return helpers.check_valid_sensitivities(counter)

app/modules/jpg.py
@@ -2,13 +2,12 @@
 
 import numpy as np
 
+import helpers
+
 def is_sensitive(filename, detector, reader):
     result = reader.readtext(str(filename), detail = 0)
     
     counter = np.array(detector.is_sensitive(result))
-    if (counter[0] > 0) or (counter[1]+counter[2] > 1) or (counter[1]+counter[3] > 1):
-        return True 
-
-    return False 
+    return helpers.check_valid_sensitivities(counter)
 
 

app/modules/pdf.py
@@ -1,51 +1,11 @@
-import os
-import PyPDF2
-
 
 import subprocess
-import sys
 
 import numpy as np
-
-def extract_text_from_pdf(pdf_path):
-    with open(pdf_path, 'rb') as file:
-        # Initialize PDF reader
-        pdf = PyPDF2.PdfReader(file)
-
-        # Get total number of pages in the PDF
-        num_pages = len(pdf.pages)
-
-        # Extract text from each page
-        text = ""
-        for page_num in range(num_pages):
-            page = pdf.pages[page_num]
-            text += page.extract_text()
-
-    return text
-
-# # Directory path
-# dir_path = "/content/"
-
-# # Get all files from the directory
-# all_files = os.listdir(dir_path)
-
-# # Filter only PDF files
-# pdf_files = [file for file in all_files if file.endswith('.pdf')]
-
-# # Process each PDF file and store extracted text
-# extracted_texts = {}
-# for pdf_file in pdf_files:
-#     extracted_texts[pdf_file] = extract_text_from_pdf(os.path.join(dir_path, pdf_file))
-
-# # Now, extracted_texts is a dictionary with filenames as keys and extracted text as values.
-# # For example, to print the extracted text from the first PDF:
-# print(list(extracted_texts.values())[0])
-
-
-
+import helpers
 
 # Maybe faster?
-def extract_text_from_pdf2(pdf_path):
+def extract_text_from_pdf(pdf_path):
     # Use subprocess to run pdftotext and capture output
     result = subprocess.run(["pdftotext", "-q", pdf_path, "-"], stdout=subprocess.PIPE)
     
@@ -58,16 +18,8 @@ def extract_text_from_pdf2(pdf_path):
 
 def is_sensitive(file_path, detector):
 
-    # text1 = extract_text_from_pdf(file_path)
-    text = extract_text_from_pdf2(file_path)
-
+    text = extract_text_from_pdf(file_path)
     
-    counter = np.array([0,0,0,0])
-    new_count = np.array(detector.is_sensitive(text))
-    counter += new_count
-    
-    if (counter[0] > 0) or (counter[1]+counter[2] > 1) or (counter[1]+counter[3] > 1):
-        return True 
-
-    return False 
+    counter = np.array(detector.is_sensitive(text))
+    return helpers.check_valid_sensitivities(counter)
     

app/modules/png.py
@@ -3,17 +3,12 @@
 import easyocr
 import numpy as np
 
+import helpers
 def is_sensitive(filename, detector):
 
     #predownload the Reader !!! 
     reader = easyocr.Reader(['en'])
     result = reader.readtext(filename, detail = 0)
 
-    counter = np.array([0,0,0,0])
-
-    new_count = np.array(detector.is_sensitive(result))
-    counter += new_count
-    if (counter[0] > 0) or (counter[1]+counter[2] > 1) or (counter[1]+counter[3] > 1):
-        return True 
-
-    return False 
+    counter = np.array(detector.is_sensitive(result))
+    return helpers.check_valid_sensitivities(counter)

app/modules/py.py
@@ -2,6 +2,8 @@
 import chardet
 import numpy as np
 
+import helpers
+
 def read_text_from_file(file_path):
     """Reads and returns the content of a file using the appropriate encoding."""
     
@@ -29,8 +31,5 @@ def is_sensitive(filename, detector):
     doc = read_text_from_file(filename)
 
     counter = np.array(detector.is_sensitive(doc))
-    if (counter[0] > 0) or (counter[1]+counter[2] > 1) or (counter[1]+counter[3] > 1):
-        return True 
-
-    return False
+    return helpers.check_valid_sensitivities(counter)
 

app/modules/txt.py
@@ -2,6 +2,8 @@
 import chardet
 import numpy as np
 
+import helpers
+
 def read_text_from_file(file_path):
     """Reads and returns the content of a file using the appropriate encoding."""
     
@@ -27,11 +29,6 @@ def is_sensitive(filename, detector):
     # here: produce list of lines  from html 
 
     doc = read_text_from_file(filename)
-    counter = np.array([0,0,0,0])
 
-    new_count = np.array(detector.is_sensitive(doc))
-    counter += new_count 
-    if (counter[0] > 0) or (counter[1]+counter[2] > 1) or (counter[1]+counter[3] > 1):
-        return True 
-
-    return False
+    counter = np.array(detector.is_sensitive(doc))
+    return helpers.check_valid_sensitivities(counter)

app/modules/zip.py
@@ -30,23 +30,6 @@ def is_sensitive(filename, detector):
     # here: produce list of lines  from html 
 
     doc = process_zip(filename)
-    counter = np.array([0,0,0,0])
 
-    for line in doc:
-        new_count = np.array(detector.is_sensitive(line))
-        counter += new_count 
-        if (counter[0] > 0) or (counter[1]+counter[2] > 1) or (counter[1]+counter[3] > 1):
-            return True 
-
-    return False
-
-# return detector.is_sensitive(text)
-# # Example
-# zip_file_path = "/content/budget-game-their.zip"
-# text_content = extract_text_from_zip(zip_file_path)
-# if text_content:
-#     print(text_content)
-# else:
-#     print("No text file found inside the zip (and nested zips).")
-
-
+    conuter = np.array(detector.is_sensitive(doc))
+    return helpers.check_valid_sensitivities(counter)