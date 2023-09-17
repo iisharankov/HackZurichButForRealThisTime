ab6ad10503babc7e8fa6d2b3025e5c3234279665
isharank <ivan.ivanov.sharankov@cern.ch>
2023-09-17 05:09:38 +0200

    reverts to old pdf scanner for testing

--- app/modules/pdf.py
+++ app/modules/pdf.py
@@ -1,11 +1,32 @@
+import os
+import PyPDF2
+
 
 import subprocess
 
 import numpy as np
-import helpers
 
-# Maybe faster?
 def extract_text_from_pdf(pdf_path):
+    with open(pdf_path, 'rb') as file:
+        # Initialize PDF reader
+        pdf = PyPDF2.PdfReader(file)
+
+        # Get total number of pages in the PDF
+        num_pages = len(pdf.pages)
+
+        # Extract text from each page
+        text = ""
+        for page_num in range(num_pages):
+            page = pdf.pages[page_num]
+            text += page.extract_text()
+
+    return text
+
+
+
+
+
+def extract_text_from_pdf_bash(pdf_path):
     # Use subprocess to run pdftotext and capture output
     result = subprocess.run(["pdftotext", "-q", pdf_path, "-"], stdout=subprocess.PIPE)
     
@@ -19,7 +40,7 @@ def extract_text_from_pdf(pdf_path):
 def is_sensitive(file_path, detector):
 
     text = extract_text_from_pdf(file_path)
-    
+
     counter = np.array(detector.is_sensitive(text))
     return helpers.check_valid_sensitivities(counter)
     
