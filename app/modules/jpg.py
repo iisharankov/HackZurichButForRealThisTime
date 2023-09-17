79732d81467dbbaffb0389f104871715b1be391e
isharank <ivan.ivanov.sharankov@cern.ch>
2023-09-17 02:07:55 +0200

    add jpg update

--- app/modules/jpg.py
+++ app/modules/jpg.py
@@ -1,21 +1,13 @@
 # Python script for jpg
 
-import easyocr
 import numpy as np
 
-def is_sensitive(filename, detector):
-
-    #predownload the Reader !!! 
-    reader = easyocr.Reader(['en'])
+def is_sensitive(filename, detector, reader):
+    
     result = reader.readtext(filename, detail = 0)
-
-    counter = np.array([0,0,0,0])
-
-    for line in result:
-        new_count = np.array(detector.is_sensitive(line))
-        counter += new_count
-        if (counter[0] > 0) or (counter[1]+counter[2] > 1) or (counter[1]+counter[3] > 1):
-            return True 
+    counter = np.array(detector.is_sensitive(result))
+    if (counter[0] > 0) or (counter[1]+counter[2] > 1) or (counter[1]+counter[3] > 1):
+        return True 
 
     return False 
 
